df47e2efede1e497eca9d15c63a893ad6b870c79
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-05 19:46:46 +0300

    add custom google map to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/widgets/custom_google_map.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CustomGoogleMap(),
+    );
   }
 }
