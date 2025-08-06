8d74dda8be6baa13add12058db01a1a6855ebb34
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-06 15:34:15 +0300

    add image and create asset file to make it easy to get images and modify it

---
+++ lib/utils/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesIconMarker
+  /// assets/images/icon_marker.png
+  static const String assetsImagesIconMarker = "assets/images/icon_marker.png";
+}
+
